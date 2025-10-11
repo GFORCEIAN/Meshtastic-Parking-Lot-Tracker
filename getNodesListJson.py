abfc5296d467d4060d50e91877d1312d99f8480f
Ian <70080915+GFORCEIAN@users.noreply.github.com>
2025-10-11 00:06:20 -0400

    added sensitive files to gitignore. added more robust bluetooth connection in getNodesListJson.py (keeps trying to connect even when another device is connected). getNodesListJson.py has a robust dictionary lookup for node id's using shortnames.

--- getNodesListJson.py
+++ getNodesListJson.py
@@ -1,28 +1,35 @@
+import asyncio
 import json
 import time
 from datetime import datetime
 from fileinput import close
 
+import bleak
 import meshtastic
 from meshtastic import ble_interface
 from meshtastic.ble_interface import BLEInterface
 
 from pubsub import pub
 
-getAllDevices = True
+getAllDevices = False
 
 filename = "newNodeReference.json"
 
 
 connected = False
 
-print("Not connected, trying to connect now...")
+print("Not connected...")
 while not connected:
     try:
+        print(" trying to connect...")
         iface = meshtastic.ble_interface.BLEInterface('Meshtastic_74cc')
         connected = True
     except meshtastic.ble_interface.BLEInterface.BLEError:
-        print("trying to connect 74CC via bluetooth")
+        pass
+    except asyncio.exceptions.TimeoutError:
+        pass
+    except bleak.exc.BleakDeviceNotFoundError:
+        pass
 print("connection successful")
 
 n = iface.nodes
@@ -46,19 +53,23 @@ if n:
     try:
         with open(filename,'r') as f:
             id = json.load(f)
+    except FileNotFoundError:
+        print("No existing Nodes to parse...")
 
-        for i in n.values():
-            if i["user"]["hwModel"] == "HELTEC_V3" or getAllDevices:
-                id.update({i["user"]["shortName"]: i["user"]["id"]})
+    for i in n.values():
+        if i["user"]["hwModel"] == "HELTEC_V3" or getAllDevices:
+            id.update({i["user"]["shortName"]: i["user"]["id"]})
 
+    try:
         with open(filename,'w') as f:
             json.dump(id, f, indent=4)
 
         print("done Writing")
+
     except IOError as e:
         print("error with file: {e}")
 
-        close()
+
 
 
 
