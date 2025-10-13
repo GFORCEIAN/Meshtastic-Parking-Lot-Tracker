8e81886fdbdbe39797cfa9253d6558bf28cb6d2a
Ian <70080915+GFORCEIAN@users.noreply.github.com>
2025-10-13 14:00:05 -0400

    updated files with some better code hopefully

--- Main.py
+++ Main.py
@@ -3,12 +3,33 @@ from pubsub import pub
 
 interface = meshtastic.serial_interface.SerialInterface()
 
+
+def interpret(s: str):
+    msg = s.split(',')
+    match(msg[0]):
+        case "L": #Update Lot Counts
+            pass #do nothing for now
+
+        case "B": #update Battery Monitoring CSV
+            pass # do nothing for now
+
+        case "E": #log error condition
+            pass # do nothing for now
+
+
+
+
+
+
+
 def onReceive(packet, interface):
     try:
         if 'decoded' in packet and packet['decoded']['portnum'] == 'TEXT_MESSAGE_APP':
             message_bytes = packet['decoded']['payload']
             message_string = message_bytes.decode('utf-8')
-            print(f"{message_string} \n> ", end="", flush=True)
+            print(message_string)
+
+            interpret(message_string)
     except KeyError as e:
         print(f"Error processing packet: {e}")
 
--- getNodesListJson.py
+++ getNodesListJson.py
@@ -1,19 +1,11 @@
-import asyncio
 import json
-import time
-from datetime import datetime
-from fileinput import close
-
-import bleak
 import meshtastic
 from meshtastic import ble_interface
-from meshtastic.ble_interface import BLEInterface
 
-from pubsub import pub
 
-getAllDevices = False
+getAllDevices = True
 
-filename = "newNodeReference.json"
+filename = "devices/newNodeReference.json"
 
 
 connected = False
@@ -22,14 +14,12 @@ print("Not connected...")
 while not connected:
     try:
         print(" trying to connect...")
-        iface = meshtastic.ble_interface.BLEInterface('Meshtastic_74cc')
+        iface = meshtastic.ble_interface.BLEInterface('Meshtastic_01c8')
         connected = True
     except meshtastic.ble_interface.BLEInterface.BLEError:
         pass
-    except asyncio.exceptions.TimeoutError:
-        pass
-    except bleak.exc.BleakDeviceNotFoundError:
-        pass
+
+        print("couldn't find specified node, still tryna connect...")
 print("connection successful")
 
 n = iface.nodes
@@ -38,32 +28,38 @@ if n:
     try:
         with open(filename,'w') as f:
            json.dump(n,f,indent=4)
+        f.close()
         print("done Writing")
     except IOError as e:
         print("error with file: {e}")
 
 
     if getAllDevices:
-        filename = "All-Devices-ids.json"
+        filename = "devices/All-Devices-ids.json"
     else:
-        filename = "Heltec-V3-Device-ids.json"
+        filename = "devices/Heltec-V3-Device-ids.json"
 
     id: dict = dict()
 
     try:
         with open(filename,'r') as f:
             id = json.load(f)
+        f.close()
     except FileNotFoundError:
         print("No existing Nodes to parse...")
 
     for i in n.values():
-        if i["user"]["hwModel"] == "HELTEC_V3" or getAllDevices:
-            id.update({i["user"]["shortName"]: i["user"]["id"]})
+        sName = i["user"]["shortName"]
+        if sName == "HELTEC_V3" or getAllDevices:
+            if  not id.keys().__contains__(sName):
+                print("found new node: " + sName + ", " + i["user"]["longName"])
+            id.update({sName : i["user"]["id"]})
+
 
     try:
         with open(filename,'w') as f:
             json.dump(id, f, indent=4)
-
+        f.close()
         print("done Writing")
 
     except IOError as e:
