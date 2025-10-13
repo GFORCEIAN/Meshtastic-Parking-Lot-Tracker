93a5881a8baee6a1b2a02a9178f9174fadeb12b2
Ian <70080915+GFORCEIAN@users.noreply.github.com>
2025-10-13 18:56:08 -0400

    created basic lot setup script, need to start writing and reading files

--- config/configMaker.py
+++ config/configMaker.py
@@ -1,13 +1,37 @@
-from logging import exception
 
-print("How Many Nodes do you Have? ", end="")
-num : int = 0
-while num <= 0:
-    try:
-        num = int(input())
-        if num <= 0:
-            print("please provide a number greater than zero", end="")
-    except ValueError as e:
-        print("please provide an integer: ", end="")
+
+
+
+def updateDevices():
+    num: int = 0
+    conf = dict()
+    conf.update({"lotConfig": {}, "serverConfig": {}, "webConfig": {}})
+
+    while num <= 0:
+        try:
+            num = int(input())
+            if num <= 0:
+                print("please provide a number greater than zero: ", end="")
+        except ValueError as e:
+            print("please provide an integer: ", end="")
+        except KeyboardInterrupt:
+            print("traitor...")
+            exit(KeyboardInterrupt())
+    for i in range(1, num + 1):
+        print("whats the short name of node " + str(i) + ": ", end="")
+        name = input()
+        print("whats the lot name " + name + " monitors?")
+        lot = input()
+        conf["lotConfig"].update({name: lot})
+    print(conf)
+
+
+
+
+
+
+
+
+
 
 
