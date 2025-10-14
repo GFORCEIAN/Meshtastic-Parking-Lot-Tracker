cda9aec2128ef16c3d690ba96fc018b615c35d61
Ian <70080915+GFORCEIAN@users.noreply.github.com>
2025-10-13 23:49:42 -0400

    updated config maker again. Not sure why I became so engrossed with this program.

--- config/configMaker.py
+++ config/configMaker.py
@@ -1,20 +1,37 @@
 import json
 
+config : dict
 
-def getLotConfig() -> dict:
-    num: int = 0
-    conf = dict({"lotConfig": {}})
-    print("How many nodes do you have? ", end="")
-    while num <= 0:
+
+def getNum() -> int:
+    num: int
+    gotNum= False
+    while not gotNum:
         try:
-            num = int(input())
-            if num <= 0:
-                print("please provide a number greater than zero: ", end="")
+            s : str = input()
+            if(s == "e"):
+                print("see ya later alligator 🐊")
+                exit(0)
+            num = int(s)
+            gotNum = True
         except ValueError as e:
             print("please provide an integer: ", end="")
         except KeyboardInterrupt:
             print("traitor...")
             raise KeyboardInterrupt("user aborted program")
+    return num
+
+def getLotConfig() -> dict:
+    num: int = 0
+    max: int = 20
+    conf = dict({"lotConfig": {}})
+    print("How many nodes do you have? ", end="")
+    while num <= 0:
+        num = getNum()
+        if num <= 0 or num > max:
+            print("please provide a number greater than zero and less than " + str(max + 1) + ": ", end="")
+            num = 0
+
     for i in range(1, num + 1):
         print("whats the short name of node " + str(i) + ": ", end="")
         name = input()
@@ -22,7 +39,26 @@ def getLotConfig() -> dict:
         lot = input()
         conf["lotConfig"].update({name: lot})
     return conf
-config : dict
+
+
+
+def getServerConfig() -> dict:
+    pass
+
+
+def getWebServerConfig() -> dict:
+    pass
+
+def updateConfigFile(newConf : dict) -> None:
+    config.update(newConf)
+    with open("config.json", "w") as w:
+        json.dump(config, w, indent=4)
+
+
+def removeDevice(s: str) -> None:
+    config["lotConfig"].pop(s)
+    updateConfigFile(config)
+
 numCommands = 5
 while True:
     op : int = 0
@@ -32,36 +68,44 @@ while True:
             j.close()
     except FileNotFoundError:
         print("no previous config found. Creating fresh config.\n")
+        config = {"lotConfig" : {}, "serverConfig" : {}, "webServerConfig" : {}}
+        updateConfigFile(config)
     print("What would you like to do?\n"
-          "1: update devices\n"
-          "2: update server configuration\n"
-          "3: update webpage configuration\n"
-          "4: see current configuration\n"
-          "5:exit\n"
+          "1: Update devices\n"
+          "2: Remove devices\n"
+          "3: Update server configuration\n"
+          "4: Update webpage configuration\n"
+          "5: See current configuration\n"
+          "Enter \"e\" to exit at any time\n"
           "> ", end="")
     while op == 0:
         try:
-            op = int(input())
+            op = getNum()
             if op <= 0 or op > numCommands:
                 op = 0
-                print("please choose a number between 1 and " + str(numCommands ) + ".")
+                print("Please choose a number between 1 and " + str(numCommands ) + ".")
         except ValueError:
-            op = 0;
+            op = 0
             print("please provide an integer")
     match op:
         case 1:
-            config.update(getLotConfig())
-            with open("config.json", "w") as w:
-                json.dump(config,w,indent=4)
+            updateConfigFile(getLotConfig())
         case 2:
-            pass
+            s = input("Whats the name of the device you want to remove? ")
+            if config["lotConfig"].keys().__contains__(s):
+                removeDevice(s)
+                print("\nRemoved: " + s + " from the configuration.\n")
+            else:
+                print("\nThat device was not Found in the current configuration.\n")
         case 3:
             pass
+            #updateConfigFile(getServerConfig())
         case 4:
-            print(json.dumps(config, indent=4))
+            pass
+            #updateConfigFile(getWebServerConfig())
         case 5:
-            print("see ya later alligator 🐊")
-            exit(0)
+            print(json.dumps(config, indent=4))
+
 
 
 
